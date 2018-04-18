5df014f1d8e3f238670ec8810eb28e9b54920070
amenraprod <amenraprod@gmail.com>
2018-04-18 04:46:56 -0400

    added typescript component

--- components/Login/index.js
+++ components/Login/index.js
@@ -19,6 +19,35 @@ export default class Login extends Component<Props> {
     super(props);
     this.state = {username: '', password: ''};
   }
+  /// <reference path="../typings/tsd.d.ts" />
+
+
+export default class Login extends React.Component<any, any> {
+    constructor(props: any){
+        super(props);
+        this.state = { name: this.props.defaultName };
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    public handleChange(event: any) : void {
+
+        this.setState({ name: event.target.value });
+    }
+
+    public render() {
+        return (
+            <div>
+                <HelloForm
+                    name = { this.state.name }
+                    handleChange = { this.handleChange }
+                />
+                <HelloContent
+                    name = { this.state.name }
+                />
+            </div>
+        );
+    }
+}
 
   render() {
     return (
